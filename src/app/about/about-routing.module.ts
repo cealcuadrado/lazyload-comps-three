d45143c58c00110ba193cc1762367bff53171d25
CeAlCuadrado <cealcuadrado@outlook.com>
2021-08-27 12:58:21 -0400

    Establecidas vistas que se abren por defecto en la aplicación

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -8,20 +8,10 @@ const routes: Routes = [
     path: "",
     component: AboutComponent,
     children: [
-      {
-        path: "home",
-        loadChildren: () =>
-          import("./home/home.module").then((m) => m.HomeModule),
-      },
-      {
-        path: "one",
-        loadChildren: () => import("./one/one.module").then((m) => m.OneModule),
-      },
-      {
-        path: "two",
-        loadChildren: () => import("./two/two.module").then((m) => m.TwoModule),
-      },
-      { path: "", redirectTo: 'home', pathMatch: 'full'},
+      { path: "home", loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),},
+      { path: "one", loadChildren: () => import("./one/one.module").then((m) => m.OneModule),},
+      { path: "two", loadChildren: () => import("./two/two.module").then((m) => m.TwoModule),},
+      { path: "", redirectTo: 'one', pathMatch: 'full'},
     ],
   },
 ];
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, { path: 'foo', loadChildren: () => import('./foo/foo.module').then(m => m.FooModule) }, { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }];
+const routes: Routes = [
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'foo', loadChildren: () => import('./foo/foo.module').then(m => m.FooModule) },
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  { path: '', redirectTo: '/about', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
